44323196b237406d6cde4cfa2d4e0d5ab83605bb
jpic <jpic@yourlabs.org>
2019-01-15 02:46:02 +0100

    Take classifiers out

--- setup.py
+++ setup.py
@@ -6,18 +6,5 @@ setup(
     url='https://yourlabs.io/oss/yash',
     setup_requires='setupmeta',
     keywords='automation cli',
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Environment :: Console',
-        'Environment :: Plugins',
-        'Intended Audience :: System Administrators',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3 :: Only',
-        'Topic :: Database',
-        'Topic :: Software Development',
-        'Topic :: System',
-        'Topic :: Terminals',
-    ],
     python_requires='>=3',
 )
